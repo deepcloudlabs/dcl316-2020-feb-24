d36b9ea1cec28138fa41e9fdbe1d687ed64be837
deepcloudlabs <deepcloudlabs@gmail.com>
2020-02-25 15:39:17 +0300

    introduce jQuery to the solution

--- mastermind-purejs/js/app.js
+++ mastermind-purejs/js/app.js
@@ -1,37 +1,22 @@
-window.onload = () => {
+$(document).ready(() => {
     let updateTable = () => {
-        emptyElement(moves);
+        $("#moves").empty();
         for (let move of game.moves) {
-            let row = moves.insertRow();
-            let cellGuess = row.insertCell(0);
-            let cellMessage = row.insertCell(1);
-            cellGuess.appendChild(
-                document.createTextNode(move.guess)
-            );
-            cellMessage.appendChild(
-                document.createTextNode(move.message)
-            );
+            $("#moves").append(`<tr><td>${move.guess}</td><td>${move.message}</td></tr>`)
         }
     }
-
     let updateView = () => {
-        tries.innerHTML = game.tries;
-        pbCounter.setAttribute("style",
-            "width: " + ((5 * game.counter) / 3) + "%;");
+        $("#tries").text(game.tries);
+        $("#pbCounter").css("width", ((5 * game.counter) / 3) + "%");
         updateTable();
     }
     let game = new GameViewModel();
-    let playBtn = document.querySelector("#play");
-    let guess = document.querySelector("#guess");
-    let tries = document.querySelector("#tries");
-    let moves = document.querySelector("#moves");
-    let pbCounter = document.querySelector("#pbCounter");
     setInterval(() => {
         game.countDown();
         updateView();
     }, 1000)
-    playBtn.onclick = () => {
-        game.play(guess.value);
+    $("#play").click(() => {
+        game.play($("#guess").val());
         updateView();
-    }
-};
+    });
+});
