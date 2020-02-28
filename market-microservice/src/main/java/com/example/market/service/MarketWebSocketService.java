893305984a9c22c8975e3f47f54f30a216b5eb10
deepcloudlabs <deepcloudlabs@gmail.com>
2020-02-28 15:10:27 +0300

    add websocket configuration and service

---
+++ market-microservice/src/main/java/com/example/market/service/MarketWebSocketService.java
@@ -0,0 +1,18 @@
+package com.example.market.service;
+
+import com.example.market.document.Trade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MarketWebSocketService {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @EventListener
+    public void listen(Trade trade){
+        messagingTemplate.convertAndSend("/topic/changes", trade);
+    }
+}
